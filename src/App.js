2153a4acb31e0559fe7c2b27696e49b1df6f1e87
thanhlong103 <thanhlong10032003@gmail.com>
2024-11-02 17:42:27 +0700

    Add Small Slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Card from './components/Card';
 import clubs2 from './images/PNG-cards-1.3/2_of_clubs.png';
+import InputWindows from './components/InputWindows';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
       <div className="card-container" style={{ position: 'absolute', top: '470px'}}>
         <Card />
         <Card />
+      </div>
+      <div style={{ position: 'absolute', left: '220px'}}>
+        <InputWindows />
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,17 @@ function Card({ image }) {
   const [isHovered, setIsHovered] = useState(false); // State to track hover status
 
   const cardStyle = {
-    width: '105px',
+    width: '105px', // Base width for small screens
     height: '150px',
-    minHeight: '150px',
+    minHeight: '130px',
     backgroundColor: '#808080',
     borderRadius: '8px',
-    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-    opacity: isHovered ? 1 : 0.6, // Change opacity based on hover status
-    transition: 'opacity 0.3s ease', // Smooth transition for opacity
+    boxShadow: isHovered ? '0px 8px 12px rgba(0, 0, 0, 0.3)' : '0px 4px 8px rgba(0, 0, 0, 0.2)', // More shadow on hover
+    opacity: isHovered ? 1 : 0.8, // Change opacity based on hover status
+    transform: isHovered ? 'scale(1.05)' : 'scale(1)', // Slight scale on hover
+    transition: 'opacity 0.3s ease, transform 0.3s ease, box-shadow 0.3s ease', // Smooth transition for effects
     backgroundImage: image ? `url(${image})` : 'none',
-    backgroundSize: 'contain',
+    backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
   };
