3a36a2b2311ca8701684a07b889f0da0bb657e33
hdungie256 <hdungie256@gmail.com>
2024-11-06 16:04:25 +0700

    fix states

--- src/components/PlayView.js
+++ src/components/PlayView.js
@@ -1,81 +1,96 @@
-import React, { useState } from "react";
+import "./PlayView.css";
 import Card from "./Card";
-import InputWindows from "./InputWindows";
 import ChooseCard from "./ChooseCard";
-import './PlayView.css'
+import InputWindows from "./InputWindows";
+import { useState } from "react";
 import Avatar from '@mui/material/Avatar';
 import opponent_avatar from '../images/avatar/opponent.png'
 import my_avatar from '../images/avatar/you.jpg'
 
-const importAll = (r) => r.keys().map(r);
-
 const avatar_size = 70;
-
 function PlayView() {
-    const [selectedCardIndex, setSelectedCardIndex] = useState(null);
-    const allCardImages = importAll(require.context('../images/PNG-cards-1.3', false, /\.(png|jpe?g|svg)$/));
     const [isPopupVisible, setIsPopupVisible] = useState(false);
-    const [cardImages, setCardImages] = useState([null, null, null, null, null, null, null]);
 
-    const handleCardClick = (index) => {
-        setSelectedCardIndex(index);
-        setIsPopupVisible(true);
-    };
+    const [river, setRiver] = useState([null, null, null, null, null]);
+    const [hole, setHole] = useState([null, null]);
 
-    const closePopup = () => {
+    const [selectedCard, setSelectedCard] = useState("");
+
+    const chooseCardImage = (image, key) => {
+        const index = parseInt(key.split("-")[1], 10);
+
+        if (key.includes("river")) {
+            setRiver((prevImages) => setNewImage(prevImages, index, image));
+        } else if (key.includes("hole")) {
+            setHole((prevImages) => setNewImage(prevImages, index, image));
+        }
         setIsPopupVisible(false);
     };
 
-    const chooseCardImage = (image) => {
-        setCardImages((prevImages) => {
-            const newImages = [...prevImages];
-            newImages[selectedCardIndex] = image;
-            return newImages;
-        });
-        closePopup();
-    };
-
     return (
-        <div className = "PlayView">
-            {/* Centered container for the first 5 cards */}
-            <div className="card-container" style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
-                {cardImages.slice(0, 5).map((image, index) => (
+        <div className="PlayView">
+            <div className="card-container"
+                 style={{
+                     display: 'flex',
+                     justifyContent: 'center',
+                     gap: '10px'
+                 }}>                {river.map((image, index) => (
+                <Card
+                    key={"river-" + index}
+                    image={image}
+                    onCardClick={() => {
+                        setIsPopupVisible(true);
+                        setSelectedCard("river-" + index);
+                    }}
+                />
+            ))}
+            </div>
+            <div className="card-container" style={{
+                display: 'flex',
+                justifyContent: 'center',
+                gap: '10px',
+                position: 'absolute',
+                top: '470px',
+                alignItems: 'center',
+            }}>
+                <Avatar alt="You" src={my_avatar} sx={{width: avatar_size, height: avatar_size}}/>
+
+                {hole.map((image, index) => (
                     <Card
-                        key={index}
+                        key={"hole" + index}
                         image={image}
-                        onCardClick={() => handleCardClick(index)}
+                        onCardClick={() => {
+                            setIsPopupVisible(true);
+                            setSelectedCard("hole-" + index);
+                        }}
                     />
                 ))}
             </div>
-
-            {/* Positioned container for the remaining cards */}
-            <div className="card-container" style={{ display: 'flex', justifyContent: 'center', gap: '10px', position: 'absolute', top: '470px', alignItems: 'center',}}>
-                <Avatar alt="You" src={my_avatar} sx={{ width: avatar_size, height: avatar_size }}/>
-
-                {cardImages.slice(5).map((image, index) => (
-                    <Card
-                        key={index + 5}
-                        image={image}
-                        onCardClick={() => handleCardClick(index + 5)}
-                    />
-                ))}
-            </div>
-
-            {/* Positioned InputWindows component */}
-            <div style={{ position: 'absolute', left: '25vw'}}>
-                <InputWindows />
+            <div style={{position: 'absolute', left: '25vw'}}>
+                <InputWindows/>
             </div>
 
             <div style={{position: 'absolute', right: '5vw'}}>
-                <Avatar alt="Opponent" src={opponent_avatar} sx={{ width: avatar_size, height: avatar_size }}/>
+                <Avatar alt="Opponent" src={opponent_avatar} sx={{width: avatar_size, height: avatar_size}}/>
             </div>
-
-            {/* Popup for selecting cards */}
             {isPopupVisible && (
-                <ChooseCard images={allCardImages} onSelect={chooseCardImage} onClose={closePopup} />
+                <ChooseCard
+                    cardKey={selectedCard}
+                    onSelect={chooseCardImage}
+                    onClose={() => {
+                        setIsPopupVisible(false);
+                        setSelectedCard("");
+                    }}
+                />
             )}
         </div>
     );
 }
 
 export default PlayView;
+
+const setNewImage = (prevImages, index, image) => {
+    const newImages = [...prevImages];
+    newImages[index] = image;
+    return newImages;
+};
