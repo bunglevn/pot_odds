cd6af0f7a0318a17c16e59b99c6eade4cfaecdf3
Nguyen Thanh Long <long.nguyen.210085@student.fulbright.edu.vn>
2024-11-12 11:34:27 +0700

    Add cards and viewmore button

--- src/components/ActionAdviceCard.js
+++ src/components/ActionAdviceCard.js
@@ -3,58 +3,58 @@ import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
 
 export function ActionAdviceCard({ title, value }) {
-    return (
-        <Card
-            sx={{
-                maxWidth: 270,
-                margin: "auto",
-                padding: 2,
-                borderRadius: 3,
-                boxShadow: 3,
-                mt: 2,
-                display: "flex",
-                flexDirection: "row",
-            }}
+  return (
+    <Card
+      sx={{
+        maxWidth: 270,
+        margin: "auto",
+        padding: 2,
+        borderRadius: 3,
+        boxShadow: 3,
+        mt: 2,
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <Grid container>
+        {/* Left half for title */}
+        <Grid
+          item
+          xs={6}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-            <Grid container>
-                {/* Left half for title */}
-                <Grid
-                    item
-                    xs={6}
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                    }}
-                >
-                    <Typography variant="subtitle1" fontWeight="bold" fontSize={20}>
-                        {title}
-                    </Typography>
-                </Grid>
+          <Typography variant="subtitle1" fontWeight="bold" fontSize={20}>
+            {title}
+          </Typography>
+        </Grid>
 
-                {/* Right half for value with red fill */}
-                <Grid
-                    item
-                    xs={6}
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        bgcolor: "red",
-                        borderRadius : 3
-                    }}
-                >
-                    <Typography
-                        variant="caption"
-                        component="div"
-                        sx={{ color: "white" }}
-                        fontWeight="bold"
-                        fontSize={25}
-                    >
-                        {value ? "Call" : "Fold"}
-                    </Typography>
-                </Grid>
-            </Grid>
-        </Card>
-    );
+        {/* Right half for value with red fill */}
+        <Grid
+          item
+          xs={6}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            bgcolor: "red",
+            borderRadius: 3,
+          }}
+        >
+          <Typography
+            variant="caption"
+            component="div"
+            sx={{ color: "white" }}
+            fontWeight="bold"
+            fontSize={25}
+          >
+            {value ? "Call" : "Fold"}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Card>
+  );
 }
--- src/components/SummaryTable.js
+++ src/components/SummaryTable.js
@@ -1,6 +1,13 @@
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
-import { Typography, Button, Dialog, DialogTitle, DialogContent, IconButton } from "@mui/material";
+import {
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  IconButton,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -11,106 +18,107 @@ import { ActionAdviceCard } from "./ActionAdviceCard";
 import { useState } from "react";
 
 const Item = styled(Paper)(({ theme }) => ({
-    height: "97.5vh",
-    backgroundColor: "#900C27",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    ...theme.applyStyles("dark", {
-        backgroundColor: "#1A2027",
-    }),
+  height: "97.5vh",
+  backgroundColor: "#900C27",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+  }),
 }));
 
 function SummaryTable({ potOdds, equity, decision }) {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <div>
-            <Item>
-                <Card sx={{ width: "100%", marginBottom: 1 }}>
-                    <CardMedia
-                        sx={{ height: 130, marginBottom: 1 }}
-                        image={summaryTableImg}
-                        title="Cover"
-                    />
-                    <CardContent>
-                        <Typography
-                            gutterBottom
-                            variant="h5"
-                            component="h1"
-                            align="center"
-                            fontWeight="bold"
-                        >
-                            Summary Table
-                        </Typography>
-                    </CardContent>
-                </Card>
+  return (
+    <div>
+      <Item>
+        <Card sx={{ width: "100%", marginBottom: 1 }}>
+          <CardMedia
+            sx={{ height: 130, marginBottom: 1 }}
+            image={summaryTableImg}
+            title="Cover"
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="h1"
+              align="center"
+              fontWeight="bold"
+            >
+              Summary Table
+            </Typography>
+          </CardContent>
+        </Card>
 
-                <CircularProgressCard title={"Pot Odds"} value={potOdds} />
+        <CircularProgressCard title={"Pot Odds"} value={potOdds} />
 
-                <CircularProgressCard title={"Equity"} value={equity} />
+        <CircularProgressCard title={"Equity"} value={equity} />
 
-                <ActionAdviceCard title={"You should"} value={decision} />
+        <ActionAdviceCard title={"You should"} value={decision} />
 
-                {/* View More button */}
-                <Button
-                    variant="contained" // You can change it to "outlined" for a border style
-                    color="primary" // You can change the color to "secondary" or custom color from theme
-                    sx={{
-                        height: 50,
-                        width: 295,
-                        mt: 2,
-                        textDecoration: "underline",
-                        borderRadius: 3, // Rounded corners
-                        padding: "10px 20px", // Adjusting padding
-                        backgroundColor: "#F6C667", // Custom background color
-                        boxShadow: 3, // Adding shadow for a 3D effect
-                        color: "#133E87",
-                        "&:hover": {
-                            backgroundColor: "#ffffff", // Darker green on hover
-                            boxShadow: 6, // Increased shadow on hover
-                        },
-                    }}
-                    onClick={handleClickOpen}
-                >
-                    View More
-                </Button>
+        {/* View More button */}
+        <Button
+          variant="contained" // You can change it to "outlined" for a border style
+          color="primary" // You can change the color to "secondary" or custom color from theme
+          sx={{
+            height: 50,
+            width: 295,
+            mt: 2,
+            textDecoration: "underline",
+            borderRadius: 3, // Rounded corners
+            padding: "10px 20px", // Adjusting padding
+            backgroundColor: "#F6C667", // Custom background color
+            boxShadow: 3, // Adding shadow for a 3D effect
+            color: "#133E87",
+            "&:hover": {
+              backgroundColor: "#ffffff", // Darker green on hover
+              boxShadow: 6, // Increased shadow on hover
+            },
+          }}
+          onClick={handleClickOpen}
+        >
+          View More
+        </Button>
 
-                {/* Popup dialog */}
-                <Dialog open={open} onClose={handleClose}>
-                    <DialogTitle>
-                        More Details
-                        <IconButton
-                            aria-label="close"
-                            onClick={handleClose}
-                            sx={{
-                                position: "absolute",
-                                right: 8,
-                                top: 8,
-                                color: (theme) => theme.palette.grey[500],
-                            }}
-                        >
-                            <CloseIcon />
-                        </IconButton>
-                    </DialogTitle>
-                    <DialogContent>
-                        <Typography variant="body1">
-                            Add additional information or details about pot odds, equity, and decision.
-                        </Typography>
-                    </DialogContent>
-                </Dialog>
-            </Item>
-        </div>
-    );
+        {/* Popup dialog */}
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>
+            More Details
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              sx={{
+                position: "absolute",
+                right: 8,
+                top: 8,
+                color: (theme) => theme.palette.grey[500],
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <Typography variant="body1">
+              Add additional information or details about pot odds, equity, and
+              decision.
+            </Typography>
+          </DialogContent>
+        </Dialog>
+      </Item>
+    </div>
+  );
 }
 
 export default SummaryTable;
